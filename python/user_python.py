2403b9fdf0a25439a264f7e8ed8f532ba3a3fa47
DevFaith <waitherafaith@outlook.com>
2024-02-25 12:50:46 +0300

    first commit

---
+++ python/user_python.py
@@ -0,0 +1,8 @@
+name=input("enter name\t")
+age=input("enter your age\n")
+location=input("enter your location\t")
+
+
+print("Hello " + name +", you are "+ age + " years old and live in "+location)
+
+
